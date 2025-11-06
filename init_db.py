6dac789a42948b31a04b7d31177b70d49e196f65
Saanvi Sharma <saanvisharma5654@gmail.com>
2025-11-06 12:01:39 +0530

    init_db.py

--- init_db.py
+++ init_db.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import sqlite3
 
-# Load the CSV
 csv_path = "defects_data.csv"
 df = pd.read_csv(csv_path)
 
-# Clean up column names (just in case)
 df.columns = [c.strip() for c in df.columns]
 
-# Convert defect_date to proper ISO format (YYYY-MM-DD)
+# Convert defect_date to proper format (YYYY-MM-DD)
 df["defect_date"] = pd.to_datetime(df["defect_date"], errors="coerce").dt.strftime("%Y-%m-%d")
 
 # Save into SQLite DB
